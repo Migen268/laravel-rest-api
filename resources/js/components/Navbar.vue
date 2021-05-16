b5d9f7d3cd0fcccf656cc70ea6e2716410b7e7dd
Migen Derstila <migen.derstila@kreatx.al>
2021-05-16 23:28:15 +0200

    started building view with vue

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="nvabar navbar-expand-sm navbar-dark bg-info mb-2">
+
+            <div class="container">
+            <a href="#" class="navbar-brand">Books</a>    
+            </div>
+    </nav>
+
+
+</template>
